ac5806bdb9cc379cf5029a1b0728004ea8602580
Walter Alves <walter.arruda.alves@gmail.com>
2017-03-17 04:54:34 -0300

    Shows Hardware information in Physical Server Summary
    Memory and Processor informations are being shown In the Physical server's summary.

--- app/helpers/physical_server_helper/textual_summary.rb
+++ app/helpers/physical_server_helper/textual_summary.rb
@@ -2,7 +2,7 @@ module PhysicalServerHelper::TextualSummary
 
 
   def textual_group_properties
-    %i(name model productName manufacturer  machineType serialNumber  uuid)
+    %i(name model productName manufacturer machineType serialNumber uuid total_memory total_cpu_cores maximum_cpu_speed)
   end
 
   def textual_group_relationships
@@ -22,31 +22,40 @@ module PhysicalServerHelper::TextualSummary
     {:label => _("Server name"), :value => @record.name }
   end
 
-   def textual_productName
-     {:label => _("Product Name"), :value => @record.productName }
-   end
-  
-   def textual_manufacturer
-     {:label => _("Manufacturer"), :value => @record.manufacturer }
-   end
-  
-  
-   def textual_machineType
-     {:label =>_("Machine Type"), :value =>  @record.machineType }
-   end
-  
-  
-   def textual_serialNumber
-     {:label => _("Serial Number"), :value => @record.serialNumber }
-   end
-  
-   def textual_uuid
-     {:label => _("UUID"), :value => @record.uuid }
-  
-   end
+  def textual_productName
+    {:label => _("Product Name"), :value => @record.productName }
+  end
 
-   def textual_model
-      {:label =>  _("Model"), :value  =>  @record.model}
-   end
-    
+  def textual_manufacturer
+    {:label => _("Manufacturer"), :value => @record.manufacturer }
+  end
+
+  def textual_machineType
+    {:label =>_("Machine Type"), :value =>  @record.machineType }
+  end
+
+  def textual_serialNumber
+    {:label => _("Serial Number"), :value => @record.serialNumber }
+  end
+
+  def textual_uuid
+    {:label => _("UUID"), :value => @record.uuid }
+  end
+
+  def textual_model
+    {:label =>  _("Model"), :value  =>  @record.model}
+  end
+
+  def textual_total_memory
+    {:label =>  _("Total Memory"), :value =>  "#{@record.hardware.memory_mb / 1024} GB"} if @record.try(:hardware)
+  end
+
+  def textual_total_cpu_cores  
+    {:label =>  _("Total CPU cores"), :value =>  "#{@record.hardware.cpu_total_cores}"} if @record.try(:hardware)
+  end
+
+  def textual_maximum_cpu_speed
+    {:label =>  _("CPU Speed"), :value =>  "#{@record.hardware.cpu_speed} GHz"} if @record.try(:hardware)
+  end 
+
 end
